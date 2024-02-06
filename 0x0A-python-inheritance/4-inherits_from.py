f66854c93aeeb26a526d3d8815274ecb01ea132b
nuatin <nuatin@yahoo.com>
2024-02-05 16:39:35 -0800

    0x0A-python-inheritance#

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 """
-Contains the inherits_from function
+ module for inherits_from.
 """
 
 
 def inherits_from(obj, a_class):
-    """returns true if obj is a subclass of a_class, otherwise false"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    """Return if the object is an instance that inherited from the specified"""
+    return isinstance(obj, a_class) and type(obj) != a_class
