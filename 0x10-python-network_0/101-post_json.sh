293280882716453f7c55b7efd6e4b9223ba261d9
nuatin <areyourorlah@gmail.com>
2024-04-25 17:46:34 -0700

    101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-# a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+url=$1
+file=$2
+response=$(curl -X POST -H "Content-Type: application/json" -d @$file $url)
+echo $response
+
+
+
+
+
 
