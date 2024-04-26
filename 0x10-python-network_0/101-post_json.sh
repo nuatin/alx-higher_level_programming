08bd8b62af451244a970068c6114e41e990520e0
nuatin <areyourorlah@gmail.com>
2024-04-25 17:49:03 -0700

    /0x10-python-network_0

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-url=$1
-file=$2
-response=$(curl -X POST -H "Content-Type: application/json" -d @$file $url)
-echo $response
-
-
-
-
-
-
+# sends a JSON POST request to a URL, and displays the body of the response.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1i"
