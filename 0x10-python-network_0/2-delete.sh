002f80f4c25a270227e510018a9a2bd529aa3beb
nuatin <areyourorlah@gmail.com>
2024-04-30 14:56:15 -0700

    123

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a DELETE request to an URL with curl and display the body of the response
-curl -sX DELETE "$1"
+# sends a DELETE request 
+curl -sL -X DELETE "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# display all HTTP methods the server will accept using curl.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# displays all HTTP methods the server will accept
+curl -si -X OPTIONS   "${1}" | grep "Allow" | cut -c 8-
